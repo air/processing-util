92501b09c980c93daafd2878e9fe8a4451df6eff
Aaron Bell <aaron.bell@gmail.com>
2011-10-13 21:50:59 -0400

    first commit

---
+++ src/air/Sketch.java
@@ -0,0 +1,27 @@
+package air;
+
+import processing.core.PApplet;
+
+public class Sketch extends PApplet {
+    protected static final long serialVersionUID = -8026930101798671837L;
+
+    protected int debugEveryNFrames = 100;
+
+    protected void showDebugAtIntervals() {
+        if (frameCount % debugEveryNFrames == 0)
+            showDebug();
+
+    }
+
+    protected void showDebug() {
+        log("%.1f", frameRate);
+    }
+
+    protected void log(String format, Object... args) {
+        System.out.println(String.format(format, args));
+    }
+
+    protected void log(String msg) {
+        System.out.println(msg);
+    }
+}
